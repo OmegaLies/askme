03c684a0edb8b4281b9a4cc68a8f2baa7541bdcc
OmegaLies <johndoe@example.com>
2022-12-13 11:17:41 +0300

    Added users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def create
+    user_params = params.require(:user).permit(:name, :nickname, :email)
+    @user = User.create(user_params)
+
+    redirect_to root_path, notice: 'Вы успешно зарегистрировались'
   end
 
   def new
