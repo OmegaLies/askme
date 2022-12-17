53f86f9932d095d13bb9abbe6efd54199293c8c2
OmegaLies <johndoe@example.com>
2022-12-17 19:05:48 +0300

    Edit and delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   def create
-    user_params = params.require(:user).permit(
-      :name, :nickname, :email, :password, :password_confirmation
-    )
     @user = User.new(user_params)
 
     if @user.save
@@ -17,5 +14,36 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.save
+      redirect_to root_path, notice: "Данные пользователя обновлены"
+    else
+      flash.now[:alert] = "При попытке сохранить пользователя возникли ошибки"
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: "Пользователь удалён"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
 
+  before_save :downcase_nickname
+
   validates :email, presence: true, uniqueness: true, confirmation: true
   validates :name, length: {maximum: 40}
-  validates :nickname, format: { with: /\A[a-z_]+\z/ }, uniqueness: true
+  validates :nickname, format: { with: /\A[A-Za-z_0-9]+\z/ }, uniqueness: true
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
