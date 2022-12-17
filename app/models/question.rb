433b3215179577ad003b0d8943e443a27e1d21ca
OmegaLies <johndoe@example.com>
2022-12-17 23:00:51 +0300

    More strong validations for email, nickname and question creation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  validates :body, presence: true, length: {maximum: 280}
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
 
   before_save :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true, confirmation: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
   validates :name, length: {maximum: 40}
-  validates :nickname, format: { with: /\A[A-Za-z_0-9]+\z/ }, uniqueness: true
+  validates :nickname, format: { with: /\A[A-Za-z_0-9]+\z/ }, length: {maximum: 40}, presence: true, uniqueness: true
 
   def downcase_nickname
     nickname.downcase!
