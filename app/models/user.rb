cb41dc2cbad020ad7ac5ce87f3c5b748dee924ca
OmegaLies <johndoe@example.com>
2022-12-17 16:59:45 +0300

    Validations for email, name, nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true, confirmation: true
+  validates :name, length: {maximum: 40}
+  validates :nickname, format: { with: /\A[a-z_]+\z/ }, uniqueness: true
 end
