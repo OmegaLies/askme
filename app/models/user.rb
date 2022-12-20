eb4bfc0586ba2c24125a554b97b4085622fd1262
OmegaLies <johndoe@example.com>
2022-12-20 11:55:14 +0300

    Header bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
   validates :name, length: {maximum: 40}
   validates :nickname, format: { with: /\A[A-Za-z_0-9]+\z/ }, length: {maximum: 40}, presence: true, uniqueness: true
+  validates :nickname, format: { with: /\A#[a-fA-f0-9]{3,6}\Z/ }, length: {maximum: 7}
 
   has_many :questions, dependent: :delete_all
 
